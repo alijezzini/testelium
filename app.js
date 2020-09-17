c6274a7f696472c8f80875c85d7034eae1884754
alijezzini <63303235+alijezzini@users.noreply.github.com>
2020-09-17 14:48:26 +0300

    Update app.js

--- app.js
+++ app.js
@@ -231,6 +231,6 @@ app.get('/',(req,res)=>{
     
 });
 app.get('*', function(req, res){
-  res.send('<h1>404 Not Found</h1>', 404);
+  res.send('<h1>404 Page Not Found</h1>', 404);
 });
 app.listen(port, () => console.log(`listening on port ${port}!`))
